dbace2533cb3435248248886e1e969b39411c837
Agustín Ramos <zentimental.software@gmail.com>
2021-06-26 12:52:59 -0500

    extracted ResourceWorker module

--- lib/model/resource.ex
+++ lib/model/resource.ex
@@ -1,4 +1,4 @@
-defmodule Resource do
+defmodule Disssim.Model.Resource do
   @keys [:id, :min_latency, :max_latency, :fail_rate]
   @enforce_keys @keys
   defstruct @keys
@@ -22,31 +22,4 @@ defmodule Resource do
     GenServer.call(pid, req)
   end
 
-  @impl true
-  def init(opts) do
-    {:ok, new(opts)}
-  end
-
-  @impl true
-  def handle_call({:request, payload}, _from, resource) do
-    req_time = delay(resource)
-    response = gen_response(payload, req_time, resource)
-    {:reply, response, resource}
-  end
-
-  defp delay(res) do
-    sleep_millis = res.min_latency + :rand.uniform(res.max_latency - res.min_latency)
-    :timer.sleep(sleep_millis)
-    sleep_millis
-  end
-
-  defp gen_response(payload, req_time, resource) do
-    case :rand.uniform() do
-      p when p <= resource.fail_rate ->
-        rand = :rand.uniform(100) |> Integer.to_string
-        {:response, payload <> "-" <> rand, req_time: req_time}
-      _ ->
-        {:error, "duh!"}
-    end
-  end
 end
---
+++ lib/model/resource_worker.ex
@@ -0,0 +1,31 @@
+defmodule Disssim.Model.ResourceWorker do
+  use GenServer
+
+  @impl true
+  def init(opts) do
+    {:ok, Disssim.Model.Resource.new(opts)}
+  end
+
+  @impl true
+  def handle_call({:request, payload}, _from, resource) do
+    req_time = delay(resource)
+    response = gen_response(payload, req_time, resource)
+    {:reply, response, resource}
+  end
+
+  defp delay(res) do
+    sleep_millis = res.min_latency + :rand.uniform(res.max_latency - res.min_latency)
+    :timer.sleep(sleep_millis)
+    sleep_millis
+  end
+
+  defp gen_response(payload, req_time, resource) do
+    case :rand.uniform() do
+      p when p <= resource.fail_rate ->
+        rand = :rand.uniform(100) |> Integer.to_string
+        {:response, payload <> "-" <> rand, req_time: req_time}
+      _ ->
+        {:error, "duh!"}
+    end
+  end
+end
