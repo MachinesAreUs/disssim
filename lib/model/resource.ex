21b503a588f4a2474be43bea785daa598c6f99d8
Agustín Ramos <zentimental.software@gmail.com>
2021-06-27 17:06:11 -0500

    Added Resource support for 'connection timeout' situations

--- lib/model/resource.ex
+++ lib/model/resource.ex
@@ -4,6 +4,7 @@ defmodule Disssim.Model.Resource do
   defstruct @keys
 
   alias Disssim.Util.Pool
+  require Logger
 
   def new(opts) do
     struct(%__MODULE__{
@@ -18,11 +19,64 @@ defmodule Disssim.Model.Resource do
   def start(opts) do
     resource = new(opts)
     {:ok, pool_pid} = Pool.create(Disssim.Model.ResourceWorker, resource, opts)
-    Agent.start(fn -> %{pool_pid: pool_pid, resource: resource} end)
+    Agent.start(fn ->
+      %{
+        pool_pid: pool_pid,
+        resource: resource,
+        stats: %{
+          total_reqs: 0,
+          curr_reqs: 0,
+          fail_reqs: 0,
+          fail_rate: 0.0
+        }
+      }
+    end)
   end
 
   def call(pid, {:request, payload} = req) when is_binary(payload) do
-    pool_id = Agent.get(pid, fn state -> state.resource.id end)
-    Pool.call(pool_id, req)
+    state = update_stats(:request, pid)
+
+    try do
+      response = Pool.call(state.resource.id, req)
+      update_stats(:response, pid, response)
+      response
+    catch
+      :exit, {:timeout, _} ->
+        Logger.warning "Request to res #{inspect(pid)} failed: Connection timeout"
+        response = {:error, :timeout}
+        update_stats(:response, pid, response)
+        response
+    end
   end
+
+  defp update_stats(:request, pid) do
+    Agent.get_and_update(pid, fn state ->
+      new_stats = %{state.stats |
+        total_reqs: state.stats.total_reqs + 1,
+        curr_reqs: state.stats.curr_reqs + 1
+      }
+      new_state = %{state | stats: new_stats}
+      {new_state, new_state}
+    end)
+  end
+
+  defp update_stats(:response, pid, response) do
+    Agent.get_and_update(pid, fn state ->
+      new_stats = %{state.stats |
+        curr_reqs: state.stats.curr_reqs - 1,
+        fail_reqs: fail_reqs(state.stats, response),
+        fail_rate: fail_rate(state.stats, response)
+      }
+      new_state = %{state | stats: new_stats}
+      {new_state, new_state}
+    end)
+  end
+
+  defp fail_reqs(stats, {:response, _, _}), do: stats.fail_reqs
+
+  defp fail_reqs(stats, {:error, _}), do: stats.fail_reqs + 1
+
+  defp fail_rate(stats, {:response, _, _}), do: stats.fail_reqs / stats.total_reqs
+
+  defp fail_rate(stats, {:error, _}), do: (stats.fail_reqs + 1) / stats.total_reqs
 end
