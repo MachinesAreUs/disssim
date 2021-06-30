578ddb6b1eff4a496e4ddaebc9676e0efa10c9ae
Agustín Ramos <zentimental.software@gmail.com>
2021-06-30 10:53:04 -0500

    Client can call resources too. Composability babes!

--- lib/model/client.ex
+++ lib/model/client.ex
@@ -1,5 +1,5 @@
 defmodule Disssim.Model.Client do
-  @keys [:id, :service, :request_rate]
+  @keys [:id, :resource, :request_rate]
   @enforce_keys @keys
   defstruct @keys
 
@@ -11,7 +11,7 @@ defmodule Disssim.Model.Client do
   def new(opts) do
     struct(%__MODULE__{
       id: UUID.uuid1(),
-      service: nil,
+      resource: nil,
       request_rate: 0 # reqs/sec
     }, opts)
   end
@@ -21,9 +21,11 @@ defmodule Disssim.Model.Client do
     Agent.start(fn -> client end)
   end
 
-  def call_once(pid, {:request, payload} = req) when is_binary(payload) do
+  def state(pid), do: Agent.get(pid, &(&1))
+
+  def call(pid, {:request, payload} = req) when is_binary(payload) do
     client = Agent.get(pid, fn client -> client end)
-    Service.call(client.service, req)
+    Service.call(client.resource, req)
   end
 
   def call_n_times(pid, {:request, payload} = req, n) when is_binary(payload) do
@@ -33,9 +35,9 @@ defmodule Disssim.Model.Client do
 
   defp call_and_wait(n, pid, client, req) do
     Logger.debug(
-      "Calling service #{inspect(client.service)} from client #{inspect(pid)} n: #{n}")
+      "Calling service #{inspect(client.resource)} from client #{inspect(pid)} n: #{n}")
 
-    task = Task.async(fn -> Service.call(client.service, req) end)
+    task = Task.async(fn -> Service.call(client.resource, req) end)
     delay = Integer.floor_div(@millis_in_second, client.request_rate)
 
     Logger.debug("Client #{inspect(pid)} waiting for #{delay} ms")
