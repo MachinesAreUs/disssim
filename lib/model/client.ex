d1e5912087aa144b985e4ff8a1aae0002eb58517
Agustín Ramos <zentimental.software@gmail.com>
2021-07-01 01:37:28 -0500

    Added basic stats to Client

--- lib/model/client.ex
+++ lib/model/client.ex
@@ -27,7 +27,8 @@ defmodule Disssim.Model.Client do
           total_reqs: 0,
           fail_reqs: 0,
           timeout_reqs: 0,
-          fail_rate: 0
+          fail_rate: 0,
+          avg_latency: 0.0
         }
       }
     end)
@@ -81,20 +82,43 @@ defmodule Disssim.Model.Client do
     end
   end
 
-  def call(pid, {:request, payload} = req) when is_binary(payload) do
-    state = state(pid)
-    client = state.client
-    Service.call(client.resource, req)
-  end
-
   defp call_and_wait(pid, client, req) do
     Logger.debug(
       "Calling #{inspect(client.resource)} from client #{inspect(pid)}")
 
-    Task.async(fn -> Service.call(client.resource, req) end)
+    Task.async(fn -> call(pid, client.resource, req) end)
     delay = trunc(@millis_in_second / client.request_rate)
 
     Logger.debug("Client #{inspect(pid)} waiting for #{delay} ms")
     :timer.sleep(delay)
+  end
+
+  defp call(pid, resource, {:request, payload} = req) when is_binary(payload) do
+    ts_start = DateTime.utc_now
+    response = Service.call(resource, req)
+    ts_end = DateTime.utc_now
+    update_stats(pid, response, ts_start, ts_end)
+  end
+
+  defp update_stats(pid, {:response, _, _}, ts_start, ts_end) do
+    Agent.update(pid, fn state ->
+      new_stats = %{state.stats |
+        total_reqs: state.stats.total_reqs + 1,
+      }
+      %{state | stats: new_stats}
+    end)
+  end
+
+  defp update_stats(pid, {:error, reason}, ts_start, ts_end) do
+    Agent.update(pid, fn state ->
+      timeout_reqs_incr = if reason == :timeout, do: 1, else: 0
+      new_stats = %{state.stats |
+        total_reqs: state.stats.total_reqs + 1,
+        fail_reqs: state.stats.fail_reqs + 1,
+        timeout_reqs: state.stats.timeout_reqs + timeout_reqs_incr,
+        fail_rate: (state.stats.fail_reqs + 1) / (state.stats.total_reqs + 1),
+      }
+      %{state | stats: new_stats}
+    end)
   end
 end
